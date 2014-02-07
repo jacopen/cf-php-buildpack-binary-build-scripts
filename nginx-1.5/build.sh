be66cb9d8a3fd962d4ce3500f6a3a39d6fea8847
Daniel Mikusa <dmikusa@gopivotal.com>
2014-02-07 09:32:33 -0800

    Upgraded to nginx 1.5.10.

--- nginx-1.5/build.sh
+++ nginx-1.5/build.sh
@@ -10,7 +10,7 @@
 ##################################################################
 #  Configuration
 #
-NGINX_VERSION=1.5.9
+NGINX_VERSION=1.5.10
 # location where files are built
 INSTALL_DIR="/tmp/staged/app"
 BUILD_DIR=`pwd`/build
