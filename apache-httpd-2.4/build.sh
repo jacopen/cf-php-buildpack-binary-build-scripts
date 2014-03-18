88140bc76b1162842326fd3df667618db8cb4507
Daniel Mikusa <dmikusa@gopivotal.com>
2014-03-17 19:10:46 -0700

    Upgraded to HTTPD 2.4.9.

--- apache-httpd-2.4/build.sh
+++ apache-httpd-2.4/build.sh
@@ -13,7 +13,7 @@
 APR_VERSION=1.5.0
 APR_ICONV_VERSION=1.2.1
 APR_UTIL_VERSION=1.5.3
-HTTPD_VERSION=2.4.7
+HTTPD_VERSION=2.4.9
 # location where files are built
 INSTALL_DIR="/tmp/staged/app"
 BUILD_DIR=`pwd`/build
