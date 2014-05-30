9477ad03387025fd333ee63a90cc10c8c5e30238
Daniel Mikusa <dmikusa@gopivotal.com>
2014-05-30 10:43:09 -0700

    Upgraded to Nginx 1.7.1.

--- nginx-1.7/build.sh
+++ nginx-1.7/build.sh
@@ -10,7 +10,7 @@
 ##################################################################
 #  Configuration
 #
-NGINX_VERSION=1.7.0
+NGINX_VERSION=1.7.1
 # location where files are built
 INSTALL_DIR="/tmp/staged/app"
 BUILD_DIR=`pwd`/build
