f0f9cff91a825c6d41df6cce8128b507b0b8b6bd
Daniel Mikusa <dmikusa@gopivotal.com>
2014-06-10 16:29:39 -0400

    Double brackets not needed.

--- build/run_remote.sh
+++ build/run_remote.sh
@@ -26,10 +26,11 @@ ROOT=$(dirname $(dirname $(readlink -e $0)))
 
 # Install git
 # TODO: make this work for other OS like CentOS
+
 ssh -t "$REMOTE_HOST" "sudo apt-get -y install git-core"
 
 # Clone or update the repo
-if ssh -q "$REMOTE_HOST" [[ -d cf-php-buildpack-binary-build-scripts ]]; then 
+if ssh -q "$REMOTE_HOST" [ -d cf-php-buildpack-binary-build-scripts ]; then 
     ssh "$REMOTE_HOST" "cd cf-php-buildpack-binary-build-scripts; git pull"
 else
     ssh "$REMOTE_HOST" "git clone https://github.com/dmikusa-pivotal/cf-php-buildpack-binary-build-scripts.git"
