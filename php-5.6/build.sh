5f2bc577b33bef57e8a5156165a56598500886cb
Daniel Mikusa <dmikusa@gopivotal.com>
2014-08-28 13:24:35 -0400

    Fixed libicu version number.

--- php-5.6/build.sh
+++ php-5.6/build.sh
@@ -140,7 +140,7 @@ package_php_extensions() {
 	package_php_extension "amqp" "$INSTALL_DIR/librmq-$RABBITMQ_C_VERSION/lib/librabbitmq.so.1"
 	package_php_extension "igbinary"
 	package_php_extension "imagick"
-	package_php_extension "intl" "libicui18n.so.42" "libicuuc.so.42" "libicudata.so.42" "libicuio.so.42"
+	package_php_extension "intl" "libicui18n.so.48" "libicuuc.so.48" "libicudata.so.48" "libicuio.so.48"
 	package_php_extension "mailparse"
 	package_php_extension "memcache"
 	package_php_extension "memcached" \
