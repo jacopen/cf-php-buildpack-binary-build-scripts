e6fae783a8ed3e7b22475adef1041047984ef2f6
Daniel Mikusa <dmikusa@pivotal.io>
2014-10-17 08:04:45 -0400

    Upgraded to PHP 5.4.34, 5.5.18 & 5.6.2.  Upgraded extensions: apcu 4.0.7, ioncube 4.6.2, mongo 1.5.7, phalcon 1.3.3, twig 1.16.1.
    Conflicts:
    	php-5.4/build.sh
    	php-5.5/build.sh

--- php-5.5/build.sh
+++ php-5.5/build.sh
@@ -10,7 +10,7 @@
 ##################################################################
 #  Configuration
 #
-PHP_VERSION=5.5.17
+PHP_VERSION=5.5.18
 VERSION_POSTFIX=
 ZTS_VERSION=20121212
 # Third Party Module Versions
@@ -22,18 +22,18 @@ MODULES[codizy]="trunk"
 MODULES[igbinary]="1.2.1"
 MODULES[imagick]="3.1.2"
 MODULES[intl]="3.0.0"
-MODULES[ioncube]="4.6.1"
+MODULES[ioncube]="4.6.2"
 MODULES[mailparse]="2.1.6"
 MODULES[memcache]="2.2.7"
 MODULES[memcached]="2.2.0"
-MODULES[mongo]="1.5.6"
+MODULES[mongo]="1.5.7"
 MODULES[msgpack]="0.5.5"
-MODULES[phalcon]="1.3.2"
 MODULES[phpiredis]="trunk"
+MODULES[phalcon]="1.3.3"
 MODULES[redis]="2.2.5"
 MODULES[suhosin]="0.9.36"
 MODULES[sundown]="0.3.11"
-MODULES[twig]="1.16.0"
+MODULES[twig]="1.16.1"
 MODULES[xcache]="3.2.0"
 MODULES[xdebug]="2.2.5"
 MODULES[xhprof]="trunk"
